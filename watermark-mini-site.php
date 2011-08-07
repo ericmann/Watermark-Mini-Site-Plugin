715d2415d122aa24cd9b6c04707fe043d7c0c0b4
Eric Mann <eric@eamann.com>
2011-08-07 16:50:59 -0700

    Add major classes from multi-network plugin

--- watermark-mini-site.php
+++ watermark-mini-site.php
@@ -10,6 +10,11 @@ Tags: multi, site, network, blog, domain
 License: GPL3+
 */
 
+/*
+ * This plugin borrows heavily and re-uses GPL-compatible code from the WordPress Multi-Network
+ * plugin written by David Dean and John James Jacoby.
+ */
+
 /* Copyright 2011  Eric Mann, Jumping Duck Media
  *
  * This program is free software; you can redistribute it and/or modify
@@ -32,18 +37,43 @@ License: GPL3+
  */
 function _watermark_mini_site_php_warning() {
 	echo '<div id="message" class="error">';
-	echo '  <p>The Daily Meds Widget requires at least PHP 5.  Your system is running version ' . PHP_VERSION . ', which is not compatible!</p>';
+	echo '  <p>The Watermark Mini-Site Plugin requires at least PHP 5.  Your system is running version ' . PHP_VERSION . ', which is not compatible!</p>';
 	echo '</div>';
 }
 
 if ( version_compare( PHP_VERSION, '5.0', '<' ) ) {
-	add_action('admin_notices', '_daily_meds_php_warning');
+	add_action('admin_notices', '_watermark_mini_site_php_warning');
 } else {
+
+	if ( !defined( 'ENABLE_NETWORK_ZERO' ) )
+		define( 'ENABLE_NETWORK_ZERO', true );
+
+	if ( !defined( 'RESCUE_ORPHANED_BLOGS' ) )
+		define( 'RESCUE_ORPHANED_BLOGS', false );
+
+	/* blog options affected by URL */
+	$options_list = array( 'siteurl', 'home', 'fileupload_url' );
+
+	/* sitemeta options to be copied on clone */
+	$options_to_copy = array(
+		'admin_email'				=> __( 'Network admin email' ),
+		'admin_user_id'				=> __( 'Admin user ID - deprecated' ),
+		'allowed_themes'			=> __( 'OLD List of allowed themes - deprecated' ),
+		'allowedthemes'				=> __( 'List of allowed themes' ),
+		'banned_email_domains'		=> __( 'Banned email domains' ),
+		'first_post'				=> __( 'Content of first post on a new blog' ),
+		'limited_email_domains'		=> __( 'Permitted email domains' ),
+		'site_admins'				=> __( 'List of network admin usernames' ),
+		'welcome_email'				=> __( 'Content of welcome email' )
+	);
+
+	define( 'NETWORKS_PER_PAGE', 10 );
+
 	// Load required class definitions.
-	require_once( 'lib/class-daily-meds.php' );
-	require_once( 'lib/class-daily-meds-widget.php' );
+	require_once( 'lib/class-watermark-mini-site.php' );
+	require_once( 'lib/class-ms-networks.php' );
 
 	// Initialize all of the plugin's hooks.
-	Daily_Meds::init();
+	Watermark_Mini_Site::init();
 }
 ?>
